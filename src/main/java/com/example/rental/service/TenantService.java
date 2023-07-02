bf7daa2a17b06fae216c81071972c4073b1aec14
VacysR <vaclovasr@yahoo.com>
2023-07-02 09:15:41 +0200

    Updated Java doc

--- src/main/java/com/example/rental/service/TenantService.java
+++ src/main/java/com/example/rental/service/TenantService.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
+/**
+ * Tenant service implementation
+ */
 @AllArgsConstructor
 @Service
 public class TenantService {
